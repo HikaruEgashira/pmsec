22ac718e568afa1dbfa8af59ff3931aad21bf574
HikaruEgashira <39324739+HikaruEgashira@users.noreply.github.com>
2026-05-08 02:29:10 +0900

    chore: bump to 0.11.0 (pmsec --doctor + MDM diagnostics)

--- python/src/pmsec/__init__.py
+++ python/src/pmsec/__init__.py
@@ -3,6 +3,6 @@ from importlib.metadata import PackageNotFoundError, version as _pkg_version
 try:
     __version__ = _pkg_version("pmsec")
 except PackageNotFoundError:
-    __version__ = "0.10.0"
+    __version__ = "0.11.0"
 
 __all__ = ["cli", "__version__"]
